bc4d2a2075b0099ab5d9e440e04c6b32901f5764
okaycomputed <100307637+okaycomputed@users.noreply.github.com>
2025-09-09 18:44:37 +0800

    variable changes again

--- frontend/api/sendmail.js
+++ frontend/api/sendmail.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: Number(process.env.PORT),
+        port: Number(process.env.SMTP_PORT),
         secure: false,
         auth: {
             user: process.env.SMTP_USERNAME,
